638b216b947e49545bfabaa67959e11e57118371
Tyler Kang <tykang8402@outlook.com>
2020-05-21 13:15:51 -0700

    restructured project around sln

--- src/Device/Midi/InputDevice.cs
+++ src/Device/Midi/InputDevice.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Runtime.InteropServices;
 
-namespace Pitcher.Midi.Devices {
+namespace Pitcher.Devices.Midi {
 
    public class InputDevice {
       
---
+++ src/Pitcher/Core.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pitcher
+{
+    class Core
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
