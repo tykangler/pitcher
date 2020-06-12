47859dc0839b158cc8da93dbe5df409c232db45f
Tyler Kang <tykang8402@outlook.com>
2020-06-11 17:35:48 -0700

    resolved changes to midi classlib in tests and messagereceiver

--- src/MessageReceiver/InputPortReceiver.cs
+++ src/MessageReceiver/InputPortReceiver.cs
@@ -10,18 +10,18 @@ namespace Pitcher.MessageReciever {
       public static void Main(string[] args) {
          AutoResetEvent resetEvent = new AutoResetEvent(false);
          const byte c6 = 72;
-         void handleMessageReceived(object _, MessageEventArgs m) {
-            if (!(m.Event is NoteEvent noteEvent && noteEvent.Type == NoteEvent.Sound.Off)) {
+         void handleMessageReceived(object? _, MessageEventArgs m) {
+            if (!(m.Event is NoteOff noteOff)) {
                PrintMessageData(m);
             }
-            if (m.Event is NoteEvent note && note.Type == NoteEvent.Sound.On) {
-               if (note.Note == c6) {
+            if (m.Event is NoteOn noteOn) {
+               if (noteOn.Note == c6) {
                   resetEvent.Set();
                }
             }
          }
          using (InputPort inputPort = new InputPort(0u)) {
-            Console.WriteLine($"Found Device {inputPort.ProductName}");
+            Console.WriteLine($"Found Device {inputPort.Device.ProductName}");
             inputPort.MessageReceived += handleMessageReceived;
             Console.WriteLine("Added Message Handler");
             inputPort.Start();
@@ -31,13 +31,11 @@ namespace Pitcher.MessageReciever {
       }
 
       static void PrintMessageData(MessageEventArgs m) {
-         if (m.Event is NoteEvent noteEvent) {
-            if (noteEvent.Type == NoteEvent.Sound.On) {
-               Console.ForegroundColor = ConsoleColor.Green;
-            } else {
-               Console.ForegroundColor = ConsoleColor.Red;
-            }
-         } 
+         Console.ForegroundColor = m.Event switch {
+            NoteOn _ => ConsoleColor.Green,
+            NoteOff _ => ConsoleColor.Red,
+            _ => ConsoleColor.White
+         };
          Console.WriteLine(m.Event?.ToString());
          Console.ForegroundColor = ConsoleColor.White;
          Console.WriteLine($"\tRaw Message: {string.Join(", ", m.Message)}");
--- test/Test.Midi/IO/InputPortTest.cs
+++ test/Test.Midi/IO/InputPortTest.cs
@@ -19,67 +19,21 @@ namespace Pitcher.Test.Midi.IO {
             output.WriteLine($"\tTimeStamp: {m.TimeStamp / 1000.0}");
         }
 
-        // void PrintNoteMessageData(MessageEventArgs m) {
-        //     NoteEvent noteEvent = m.Event as NoteEvent;
-        //     output.WriteLine($"Type: Note {noteEvent.Type}");
-        //     PrintBasicMessageData(m);
-        //     output.WriteLine($"Note: {noteEvent.Note}");
-        //     output.WriteLine($"Velocity: {noteEvent.Velocity}");
-        // }
-
-        // void PrintProgramChangeMessageData(MessageEventArgs m) {
-        //     ProgramChange programChange = m.Event as ProgramChange;
-        //     output.WriteLine("Type: ProgramChange")
-        //     PrintBasicMessageData(m);
-        //     output.WriteLine($"Program: {programChange.Program}");
-        // }
-
-        // void PrintChannelPressureMessageData(MessageEventArgs m) {
-        //     ChannelPressure channelPressure = m.Event as ChannelPressure;
-        //     output.WriteLine("Type: ChannelPressure");
-        //     PrintBasicMessageData(m);
-        //     output.WriteLine($"Pressure: {channelPressure.Pressure}");
-        // }
-
-        // void PrintControllerMessageData(MessageEventArgs m) {
-        //     Controller controller = m.Event as Controller;
-        //     output.WriteLine("Type: Controller");
-        //     PrintBasicMessageData(m);
-        //     output.WriteLine($"ControlDevice: {controller.Device}");
-        //     output.WriteLine($"Which: {controller.Which}");
-        // }
-        
-        // void PrintPitchBendMessageData(MessageEventArgs m) {
-        //     PitchBend pitchBend = m.Event as PitchBend;
-        //     output.WriteLine("Type: PitchBend");
-        //     PrintBasicMessageData(m);
-        //     output.WriteLine($"Bend: {pitchBend.Bend}");
-        // }
-
-        // void PrintPolyphonicPressureMessageData(MessageEventArgs m) {
-        //     PolyphonicPressure polyphonicPressure = m.Event as PolyphonicPressure;
-        //     output.WriteLine("Type: PolyphonicPressure");
-        //     PrintBasicMessageData(m);
-        //     output.WriteLine($"Note: {polyphonicPressure.Note}");
-        //     output.WriteLine($"Pressure: {polyphonicPressure.Pressure}");
-        // }
-
-
         [Fact]
         /// <remark>require that at least one device is connected</remark>
         public void ConstructFillsProperties() {
             InputPort inputPort = new InputPort(0u);
-            Assert.True(inputPort.DeviceId == 0u);
-            Assert.True(inputPort.ProductName.Length != 0);
-            output.WriteLine("Product Name: " + inputPort.ProductName);
-            output.WriteLine("Product ID: " + inputPort.ProductId);
+            Assert.True(inputPort.Device.DeviceId == 0u);
+            Assert.True(inputPort.Device.ProductName.Length != 0);
+            output.WriteLine("Product Name: " + inputPort.Device.ProductName);
+            output.WriteLine("Product ID: " + inputPort.Device.ProductId);
         }
 
         [Fact]
         public void StartHandlesSingleNoteEvent() {
             AutoResetEvent resetEvent = new AutoResetEvent(false);
             void handleMessageReceived(object _, MessageEventArgs m) {
-                Assert.IsType<NoteEvent>(m.Event);
+                Assert.IsAssignableFrom<NoteEvent>(m.Event);
                 PrintMessageData(m);
                 resetEvent.Set();
             }
@@ -94,7 +48,7 @@ namespace Pitcher.Test.Midi.IO {
         public void StartHandlesMultipleNoteEvents() {
             CountdownEvent countdown = new CountdownEvent(5);
             void handleMessageReceived(object _, MessageEventArgs m) {
-                Assert.IsType<NoteEvent>(m.Event);
+                Assert.IsAssignableFrom<NoteEvent>(m.Event);
                 PrintMessageData(m);
                 countdown.Signal();
             }
